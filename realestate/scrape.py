df85b3f72a76e55a909a2d5bbb873faaa716f1e6
taz <tsk.nakamura@gmail.com>
2020-11-22 21:59:40 +0900

    scrape

--- realestate/scrape.py
+++ realestate/scrape.py
@@ -1,15 +1,18 @@
-
 import pandas as pd
 from urllib import request
 import re
 from lxml import etree, html
 import urllib.parse
 
-url = "https://www.rakumachi.jp/syuuekibukken/area/prefecture/dimAll/?area[]=1101&area[]=1102&area[]=1103&area[]=1104&area[]=1105&area[]=1106&area[]=1107&area[]=1108&area[]=1109&area[]=1110&newly=&price_from=&price_to=&gross_from=&gross_to=&dim[]=1002&dim[]=1004&year=&b_area_from=&b_area_to=&houses_ge=&houses_le=&min=&l_area_from=&l_area_to=&keyword=&ex_real_search="
+# url = "https://www.rakumachi.jp/syuuekibukken/area/prefecture/dimAll/?area[]=1101&area[]=1102&area[]=1103&area[]=1104&area[]=1105&area[]=1106&area[]=1107&area[]=1108&area[]=1109&area[]=1110&newly=&price_from=&price_to=&gross_from=&gross_to=&dim[]=1002&dim[]=1004&year=&b_area_from=&b_area_to=&houses_ge=&houses_le=&min=&l_area_from=&l_area_to=&keyword=&ex_real_search="
+url = "https://www.rakumachi.jp/syuuekibukken/area/prefecture/dimAll/?area%5B%5D=1&newly=&price_from=&price_to=&gross_from=&gross_to=&dim%5B%5D=1002&dim%5B%5D=1004&year=&b_area_from=&b_area_to=&houses_ge=&houses_le=&min=&l_area_from=&l_area_to=&keyword=&ex_real_search="
 
-Agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
 
-H = {'User-Agent': Agent, 'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3'}
+HEADERS = {
+    'User-Agent': USER_AGENT,
+    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3'
+}
 
 GREEN = '\x1b[32;21m'
 PURPLE = '\x1b[32;35m'
@@ -20,69 +23,64 @@ def debug(*msg):
     print(PURPLE, *msg, RESET)
 
 
-def get_page(url):
+def get_html(url):
     try:
-        req = request.Request(url, headers=H)
+        req = request.Request(url, headers=HEADERS)
         f = request.urlopen(req, timeout=60)
     except Exception as e:
         debug(e, url)
 
     assert(f.status == 200)
+    
     charset = f.headers.get_content_charset()
-    charset
-    raw = f.read()
+    raw = f.read().decode(charset, errors="replace")
 
-    return raw.decode(charset, errors="replace")
+    return html.fromstring(raw)
 
-
-parts = urllib.parse.urlparse(url)
-base_url = "{}://{}{}".format(parts.scheme, parts.netloc, parts.path)
-
-# main/1st page
-raw = get_page(url)
-houses = html.fromstring(raw)
 
 # get node with house info
-info_rule = '//div[@class="propertyBlock__contentRight"]'
+INFO_RULE = '//div[@class="propertyBlock__contentRight"]'
 # easier to just get 'rendered text' with .text_content() then use regex
 # partial implementation
-regex_map = {
+REGEX_MAP = {
     "price": r"(?<=価格).+\s([\d\w]+万)(?:\s+)?(?=円)",
-    "gross": r"(?<=利回り).+\s([\d\.%]+)(?=\s)",
+    "yield": r"(?<=利回り).+\s([\d\.%]+)(?=\s)",
     "address": r"(?<=所在地)(.*)(?=沿線交通)",
-    "floor_area": r"(?<=建物面積)[\s]+([\d\.]+)(?=㎡)",
+    "date_of_construction": r"(?<=築年月)(.*)(?=総戸数)",
+    "total_units": r"(?<=総戸数)(.*)(?=建物構造)",
+    "building_structure": r"(?<=建物構造)(.*)(?=建物面積)",
+    "building_area": r"(?<=建物面積)[\s]+([\d\.]+)(?=㎡)",
+    "num_floors": r"(?<=階数)(.*)(?=土地面積)",
     "land_area": r"(?<=土地面積)[\s]+([\d\.]+)(?=㎡)"
 }
 
-
-def get_data(houses):
+def get_data(page):
     results = []
-    for info in houses.xpath(info_rule):
+    for info in page.xpath(INFO_RULE):
         txt = info.text_content().replace("\n", "")
         data = {}
-        for k, v in regex_map.items():
+        for k, v in REGEX_MAP.items():
             m = re.search(v, txt, flags=re.MULTILINE)
             if m:
                 data[k] = m.group(1).strip()
         results.append(data)
     return results
 
+### main ###
+parts = urllib.parse.urlparse(url)
+base_url = "{}://{}{}".format(parts.scheme, parts.netloc, parts.path)
 
-# first page results 20
-results = get_data(houses)
-print(len(results))
+first_page = get_html(url)
 
-# this should really be parallelized into different processes
-links_to_more_pages = '//div[@class="header_pager_navigation"]//li[contains(@id, "pagination_page_")]/a/@href'
+LINKS_TO_MORE_PAGES = '//div[@class="header_pager_navigation"]//li[contains(@id, "pagination_page_")]/a/@href'
+links = first_page.xpath(LINKS_TO_MORE_PAGES)
 # go to other pages(links) found in 1st page
-for u in houses.xpath(links_to_more_pages):
-    u = base_url + u
-    raw = get_page(u)
-    houses = html.fromstring(raw)
-    results.extend(get_data(houses))
+results = []
+i = 1
+for link in links:
+    print("page", i)
+    i += 1
+    page = get_html(base_url + link)
+    results.extend(get_data(page))
 
-print(len(results))
-
-
-units = pd.DataFrame(results)
-units.to_csv("rakumachi.csv", index=False)
+pd.DataFrame(results).to_csv("rakumachi.csv", index=False)
