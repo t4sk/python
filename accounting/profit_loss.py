8845853d8607e69ae2e0e2b2c8d5097358ad11a3
taz <tsk.nakamura@gmail.com>
2020-12-14 20:55:07 +0900

    profit loss

--- accounting/profit_loss.py
+++ accounting/profit_loss.py
@@ -3,9 +3,11 @@
 from lib import csv_to_t_accounts, create_balance_sheet, check_balance_sheet, print_profit_loss
 
 def main(**kwargs):
+    check_balance = kwargs.get("check_balance", True)
     t_accounts = csv_to_t_accounts(kwargs["t_accounts_csv"])
 
     balance_sheet = create_balance_sheet(t_accounts)
-    check_balance_sheet(balance_sheet)
+    if check_balance:
+        check_balance_sheet(balance_sheet)
 
     print_profit_loss(balance_sheet)
